b3086426dacd84cc34295dc567a8e9ccfc4951c4
Evoiz <evoiz963@gmail.com>
2025-02-18 21:36:43 +0300

    Fix udp

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -7,170 +7,187 @@
 
 #define MAX_ROOMS 5
 #define SENSOR_INTERVAL 2000
-#define UDP_INTERVAL 100
-#define ESP_NAME "SmartEnvironmentHub"
+#define UDP_PORT 1234
+#define ESP_NAME "EnvironmentHub"
+#define MAX_GPIO 16
+#define JSON_SIZE 128
 
-//const char* ssid = "Kali";
-//const char* password = "y=e^(cos(Xy))";
+const char* ssid = "Kali";
+const char* password = "y=e^(cos(xy))";
 
-//const char* ssid = "Hnndes";
-//const char* password = "58KMCHqR";
-
-const char* ssid = "evoiz";
-const char* password = "eee1998eee";
-
-class Room {
-  private:
-    int dhtPin;
-    int gasPin;
-    DHT* dht = nullptr;
-    
-  public:
-    float temperature;
-    float humidity;
-    int gasValue;
-    
-    Room(int dht_pin = -1, int gas_pin = -1) : 
-      dhtPin(dht_pin), gasPin(gas_pin) {
-      if (dht_pin != -1) {
-        dht = new DHT(dht_pin, DHT22);
-      }
-    }
-
-    ~Room() {
-      if (dht != nullptr) {
-        delete dht;
-      }
-    }
-
-    void begin() {
-      pinMode(gasPin, INPUT);
-      if (dht != nullptr) {
-        dht->begin();
-      }
-    }
-
-    void readSensors() {
-      if (dht != nullptr) {
-        temperature = dht->readTemperature();
-        humidity = dht->readHumidity();
-      } else {
-        temperature = -1;
-        humidity = -1;
-      }
-      gasValue = analogRead(gasPin);
-      
-      if (isnan(temperature) || isnan(humidity)) {
-        temperature = -1;
-        humidity = -1;
-      }
-    }
+struct RoomConfig {
+  int8_t dht_pin = -1;
+  int8_t gas_pin = -1;
+  bool initialized = false;
 };
 
-Room rooms[MAX_ROOMS];
-int active_rooms = 0;
+class SafeRoom {
+private:
+  DHT* dht = nullptr;
+  RoomConfig config;
+
+  bool validGPIO(int8_t pin) { return pin >= 0 && pin <= MAX_GPIO; }
+  
+public:
+  float temperature = NAN;
+  float humidity = NAN;
+  int gas_value = -1;
+
+  SafeRoom() = default;
+
+  bool begin(const RoomConfig& cfg) {
+    if(config.initialized) return false;
+    
+    config = cfg;
+    if(validGPIO(config.dht_pin)) {
+      dht = new DHT(config.dht_pin, DHT22);
+      dht->begin();
+    }
+    if(validGPIO(config.gas_pin)) {
+      pinMode(config.gas_pin, INPUT);
+    }
+    config.initialized = true;
+    return true;
+  }
+
+  void read() {
+    if(dht) {
+      temperature = dht->readTemperature(false);
+      humidity = dht->readHumidity(false);
+      yield();
+    }
+    if(validGPIO(config.gas_pin)) {
+      gas_value = analogRead(config.gas_pin);
+    }
+  }
+
+  ~SafeRoom() {
+    if(dht) delete dht;
+  }
+};
+
+SafeRoom rooms[MAX_ROOMS];
+uint8_t active_rooms = 0;
 
 ESP8266WebServer server(80);
 WiFiUDP udp;
 Ticker sensorTicker;
-Ticker udpTicker;
+
+void setupNetwork() {
+  WiFi.persistent(true);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    yield();
+  }
+}
 
 void handleCreateRoom();
-void handleRoomRequest();
-void checkUDPMessages();
+void handleGetRoom();
+void handleUDP();
+bool validatePins(int8_t dht, int8_t gas);
 
 void setup() {
   Serial.begin(115200);
+  setupNetwork();
   
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
-  Serial.println("Connected! IP: " + WiFi.localIP().toString());
-
-  server.on("/createRoom", HTTP_POST, handleCreateRoom);
-  server.on("/room{room_num}", HTTP_GET, handleRoomRequest);
+  server.on("/room", HTTP_POST, handleCreateRoom);
+  server.on("/room/{id}", HTTP_GET, handleGetRoom);
   server.begin();
 
-  sensorTicker.attach_ms(SENSOR_INTERVAL, []() {
-    for(int i=0; i<active_rooms; i++) rooms[i].readSensors();
+  sensorTicker.attach_ms(SENSOR_INTERVAL, [](){
+    for(uint8_t i=0; i<active_rooms; i++) {
+      if(rooms[i].temperature || rooms[i].humidity) { // Force no-op optimization
+        rooms[i].read();
+      }
+    }
   });
 
-  udpTicker.attach_ms(UDP_INTERVAL, []() {
-    checkUDPMessages();
-  });
-  udp.begin(1234);
+  udp.begin(UDP_PORT);
+  Serial.printf("\nSystem Ready\nIP: %s\n", WiFi.localIP().toString().c_str());
 }
 
 void loop() {
   server.handleClient();
-  if (WiFi.status() != WL_CONNECTED) WiFi.reconnect();
+  handleUDP();
+  yield();
 }
 
-void handleRoomRequest() {
-  int room_num = server.uri().substring(server.uri().lastIndexOf('m')+1).toInt();
-  if(room_num <1 || room_num >active_rooms) {
-    server.send(404, "text/plain", "Invalid room number");
+void handleGetRoom() {
+  uint8_t id = server.pathArg(0).toInt();
+  if(id == 0 || id > active_rooms) {
+    server.send(400, "text/plain", "Invalid ID");
     return;
   }
 
-  DynamicJsonDocument doc(256);
-  Room& room = rooms[room_num-1];
-  doc["room"] = room_num;
-  doc["temperature"] = room.temperature;
-  doc["humidity"] = room.humidity;
-  doc["gas"] = room.gasValue;
+  StaticJsonDocument<JSON_SIZE> doc;
+  SafeRoom& room = rooms[id-1];
+  
+  if(!isnan(room.temperature)) doc["temp"] = room.temperature;
+  if(!isnan(room.humidity)) doc["hum"] = room.humidity;
+  if(room.gas_value != -1) doc["gas"] = room.gas_value;
 
-  String response;
-  serializeJson(doc, response);
+  char response[JSON_SIZE];
+  serializeJson(doc, response, sizeof(response));
   server.send(200, "application/json", response);
 }
 
-void checkUDPMessages() {
+void handleCreateRoom() {
+  StaticJsonDocument<JSON_SIZE> doc;
+  if(deserializeJson(doc, server.arg("plain")) != DeserializationError::Ok) {
+    server.send(400, "text/plain", "Bad JSON");
+    return;
+  }
+
+  RoomConfig cfg{
+    doc["dht_pin"] | -1,
+    doc["gas_pin"] | -1
+  };
+
+  if(!validatePins(cfg.dht_pin, cfg.gas_pin) || active_rooms >= MAX_ROOMS) {
+    server.send(400, "text/plain", "Invalid pins");
+    return;
+  }
+
+  if(!rooms[active_rooms].begin(cfg)) {
+    server.send(500, "text/plain", "Init failed");
+    return;
+  }
+
+  active_rooms++;
+  
+  StaticJsonDocument<JSON_SIZE> res;
+  res["id"] = active_rooms;
+  res["status"] = "created";
+  
+  char output[JSON_SIZE];
+  serializeJson(res, output, sizeof(output));
+  server.send(201, "application/json", output);
+}
+
+void handleUDP() {
   int packetSize = udp.parsePacket();
-  String message = udp.readStringUntil('\n');
-  if(packetSize && message == "DISCOVER_DEVICES") {
-    String response = String(ESP_NAME) + "," +
+  if(packetSize) {
+    String message = udp.readStringUntil('\n');
+    
+    if(message == "DISCOVER_DEVICES") {
+      String response = String(ESP_NAME) + "," +
                      WiFi.macAddress() + "," +
                      WiFi.localIP().toString() + "," +
                      String(active_rooms);
-    
-    udp.beginPacket(udp.remoteIP(), udp.remotePort());
-    udp.print(response);
-    udp.endPacket();
+      
+      udp.beginPacket(udp.remoteIP(), udp.remotePort());
+      udp.print(response);
+      udp.endPacket();
+    }
   }
 }
 
-void handleCreateRoom() {
-  if (server.method() != HTTP_POST) {
-    server.send(405, "text/plain", "Method Not Allowed");
-    return;
-  }
-
-  DynamicJsonDocument doc(256);
-  DeserializationError error = deserializeJson(doc, server.arg("plain"));
-  
-  if (error) {
-    server.send(400, "text/plain", "Invalid JSON");
-    return;
-  }
-
-  int dht_pin = doc["dht_pin"];
-  int gas_pin = doc["gas_pin"];
-  
-  if (active_rooms >= MAX_ROOMS) {
-    server.send(507, "application/json", "{\"status\":\"MAX_ROOMS_REACHED\"}");
-    return;
-  }
-
-  rooms[active_rooms] = Room(dht_pin, gas_pin);
-  rooms[active_rooms].begin();
-  active_rooms++;
-
-  DynamicJsonDocument responseDoc(256);
-  responseDoc["status"] = "SUCCESS";
-  responseDoc["room_number"] = active_rooms;
-  responseDoc["message"] = "Room created successfully";
-  
-  String response;
-  serializeJson(responseDoc, response);
-  server.send(201, "application/json", response);
+bool validatePins(int8_t dht, int8_t gas) {
+  bool valid = true;
+  if(dht != -1) valid &= (dht >= 0 && dht <= MAX_GPIO);
+  if(gas != -1) valid &= (gas >= 0 && gas <= MAX_GPIO);
+  return valid;
 }
+
